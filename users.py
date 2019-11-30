d0cc0a6b2463bbabd5b9db6ad761599bc481442d
Julien Berthomier <julien@getstation.com>
2019-11-30 13:36:52 +0100

    Users migration

--- users.py
+++ users.py
@@ -1,13 +1,15 @@
 
 import requests
 import json
+import time
 
 # Get list of users
 
 users = []
+users_info = []
 
 i = 0
-while i < 4:
+while i < 100:
     i = i + 1
 
     endpoint_users = "https://community.getstation.com/admin/users/list/active.json?show_emails=true"
@@ -25,47 +27,52 @@ while i < 4:
 
     users.append(paged_users)
 
+    print(len(paged_users))
+
     # Specific variables to extract
-    users_info = []
     for d in paged_users:
         info = {
         "userID":d['id'],
-        "name":d['name'],
-        "username":d['username'],
+        "name":d['name'] if d['name'] else d['username'],
         "email":d['email'],
         "created":d['created_at'],
         "avatarURL":"https://community.getstation.com" + d['avatar_template'].replace("{size}", "240")
         }
         users_info.append(info)
 
-# create a formatted string of the JSON object
-        def jprint(obj):
-            text = json.dumps(obj, sort_keys=True, indent=4)
-            print(text)
+    nb_users_exported = len(paged_users)
 
-        #jprint(users_info)
+    if nb_users_exported < 100:
+        break
 
-        jprint(users_info)
+# Create a formatted string of the JSON object
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
-print(len(users))
+# jprint(users_info)
 
-# import data to Canny endpoint
+print(len(users_info))
+
+# Import data to Canny endpoint
 
 endpoint_canny = "https://station.canny.io/api/v1/users/find_or_create"
 
 payload2 = {
-'apiKey': "292eb22b-a1f2-cf73-d5a1-7ce005ffc4b2"
+'apiKey': '292eb22b-a1f2-cf73-d5a1-7ce005ffc4b2'
 }
 
-my_data = {
-"avatarURL": "https://community.getstation.com/user_avatar/community.getstation.com/julien/240/66_2.png",
-"created": "2017-10-20T04:27:45.769Z",
-"email": "julien@getstation.com",
-"name": "Julien Berthomier",
-"userID": 13,
-"username": "julien"
-}
+count = 0
 
-import_canny = requests.post(endpoint_canny, data=my_data, params=payload2)
+for user in users_info:
 
-print(import_canny.status_code)
+    import_canny = requests.post(endpoint_canny, params=payload2, data=user)
+
+    print(import_canny.content)
+
+    count = count+1
+    print('count ' + str(count))
+    if count == 25:
+        print("sleeping")
+        time.sleep(2)
+        count = 0
