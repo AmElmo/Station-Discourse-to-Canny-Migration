4a44fe252f3e0022cdfbb65828926b1d305e6e44
Julien Berthomier <57484407+AmElmo@users.noreply.github.com>
2020-01-10 11:10:21 +0100

    Update list_boards_canny.py

--- list_boards_canny.py
+++ list_boards_canny.py
@@ -7,7 +7,7 @@ import json
 endpoint_canny = "https://canny.io/api/v1/boards/list"
 
 payload2 = {
-'apiKey': '292eb22b-a1f2-cf73-d5a1-7ce005ffc4b2'
+'apiKey': '???'
 }
 
 import_canny = requests.post(endpoint_canny, params=payload2)
